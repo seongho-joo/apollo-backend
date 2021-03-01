65d04717b4bf127b431a2fffd0692962267c487d
seongho-joo <hazzyseong@gmail.com>
2021-03-01 20:51:28 +0900

    #4.15 파일업로드드

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
-import schema from './schema';
-import { getUser, protectResolver } from './users/users.utils';
+import { typeDefs, resolvers } from './schema';
+import { getUser } from './users/users.utils';
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
-  schema,
+  typeDefs,
+  resolvers,
   context: async ({ req }) => {
-    return { loggedInUser: await getUser(req.headers.token), protectResolver };
+    return { loggedInUser: await getUser(req.headers.token) };
   },
 });
 
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,39 +2,40 @@ import client from '../../client';
 import bcrypt from 'bcrypt';
 import { protectedResolver } from '../users.utils';
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio },
+  { loggedInUser }
+) => {
+  let hash = null;
+  if (newPassword) {
+    hash = await bcrypt.hash(newPassword, 10);
+  }
+  const updateUser = await client.user.update({
+    where: { id: loggedInUser.id },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      bio,
+      ...(hash && { password: hash }),
+    },
+  });
+  if (updateUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: 'Could not profile.',
+    };
+  }
+};
+
 export default {
   Mutation: {
-    edtitProfile: protectedResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser, protectResolver }
-      ) => {
-        let hash = null;
-        if (newPassword) {
-          hash = await bcrypt.hash(newPassword, 10);
-        }
-        const updateUser = await client.user.update({
-          where: { id: loggedInUser.id },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(hash && { password: hash }),
-          },
-        });
-        if (updateUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: 'Could not profile.',
-          };
-        }
-      }
-    ),
+    edtitProfile: protectedResolver(resolverFn),
   },
 };
