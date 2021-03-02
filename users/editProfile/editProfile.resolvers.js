ab8b8b975de787e121df1bb1c775ebf2a11873bd
seongho-joo <hazzyseong@gmail.com>
2021-03-02 18:10:34 +0900

    #4.16-17 file upload

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,12 +1,17 @@
+import { createWriteStream } from 'fs';
 import client from '../../client';
 import bcrypt from 'bcrypt';
 import { protectedResolver } from '../users.utils';
 
 const resolverFn = async (
   _,
-  { firstName, lastName, username, email, password: newPassword, bio },
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser }
 ) => {
+  const { filename, createReadStream } = await avatar;
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(process.cwd() + '/uploads/' + filename);
+  readStream.pipe(writeStream);
   let hash = null;
   if (newPassword) {
     hash = await bcrypt.hash(newPassword, 10);
