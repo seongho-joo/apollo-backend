bf0427be0cd667827b37edcaa5ac55aa3480f755
seongho-joo <hazzyseong@gmail.com>
2021-03-04 20:24:39 +0900

    unfollow User

---
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -0,0 +1,11 @@
+const { gql } = require('apollo-server');
+
+export default gql`
+  type UnfollowUserResult {
+    ok: Boolean!
+    error: String
+  }
+  type Mutation {
+    unfollowUser(username: String!) UnfollowUserResult!
+  }
+`;
