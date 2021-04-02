7160da9a396a61001a74729442b5459aa0e96d84
seongho-joo <hazzyseong@gmail.com>
2021-04-02 16:16:40 +0900

    message 로직 추가

--- src/messages/messages.resolvers.ts
+++ src/messages/messages.resolvers.ts
@@ -21,6 +21,10 @@ const resolvers: Resolvers = {
       });
     },
   },
+  Message: {
+    user: ({ id }, _, { client }) =>
+      client.message.findUnique({ where: { id } }).user(),
+  },
 };
 
 export default resolvers;
